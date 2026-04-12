74b0be8ae24732f75d691b134278f4c50a18b399
soldout123 <linton757@gmail.com>
2026-04-11 21:12:42 -0500

    Fix TWA Location Delegation Engine

--- twa-source/app/src/main/java/com/swiftdeal/app/DelegationService.java
+++ twa-source/app/src/main/java/com/swiftdeal/app/DelegationService.java
@@ -1,22 +1,11 @@
 package com.swiftdeal.app;
 
-
 import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
 
-import com.google.androidbrowserhelper.playbilling.digitalgoods.DigitalGoodsRequestHandler;
-
-
-public class DelegationService extends
-        com.google.androidbrowserhelper.trusted.DelegationService {
+public class DelegationService extends com.google.androidbrowserhelper.trusted.DelegationService {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        
-            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
-        
-            registerExtraCommandHandler(new DigitalGoodsRequestHandler(getApplicationContext()));
-        
+        registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
     }
 }
-
